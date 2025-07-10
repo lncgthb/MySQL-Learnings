4a78080f8b7bd06742240bbe60460126a2e05e64
Lnc <220098620+lncgthb@users.noreply.github.com>
2025-07-10 17:12:55 -0400

    Update 2. WHERE - DATA TYPES - LOGICAL OPERATORS - LIKE statement.sql

--- 1. MySQL Beginner/2. WHERE - DATA TYPES - LOGICAL OPERATORS - LIKE statement.sql
+++ 1. MySQL Beginner/2. WHERE - DATA TYPES - LOGICAL OPERATORS - LIKE statement.sql
@@ -4,8 +4,8 @@ FROM parks_and_recreation.employee_salary
 WHERE first_name = 'Leslie'
 ;
 
-# DATA TYPES + - * / = !=
-# LOGICAL OPERATORS  and or not
+# DATA TYPES (+ - * / = !=)
+# LOGICAL OPERATORS  (and or not)
 SELECT *
 FROM parks_and_recreation.employee_salary
 WHERE salary <= 50000
@@ -26,7 +26,7 @@ OR NOT gender = 'Male'
 # LIKE Statement   (% = anything) (_ = specific value)
 SELECT *
 FROM parks_and_recreation.employee_demographics
-WHERE first_name LIKE 'a___%'   -- '%er%'     'er%'     'a__'
+WHERE first_name LIKE 'a___%'  -- other examples ('%er%'     'er%'     'a__')
 OR birth_date LIKE '1988%'
 ;
 
